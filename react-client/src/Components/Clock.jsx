75cf21267249be98788225585f82a1034b22174f
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-11-12 01:15:52 -0800

    Add components and handler

--- react-client/src/Components/Clock.jsx
+++ react-client/src/Components/Clock.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import AlarmClockForm from './AlarmClockForm.jsx';
 
 class Clock extends Component {
   constructor(props) {
@@ -11,13 +12,25 @@ class Clock extends Component {
 
   render() {
     return (
-      <div className="clock">
-        <span className="clock-child hours">{this.prependZero(this.props.hours)}</span>:
-        <span className="clock-child minutes">{this.prependZero(this.props.minutes)}</span>:
-        <span className="clock-child seconds">{this.prependZero(this.props.seconds)}</span>
-        <span className="clock-child period">{this.props.period}</span>
+      <div className="clock-container">
+        <div className="clock-container-top">
+          <div className="clock">
+            <span className="clock-child hours">{this.prependZero(this.props.hours)}</span>:
+            <span className="clock-child minutes">{this.prependZero(this.props.minutes)}</span>:
+            <span className="clock-child seconds">{this.prependZero(this.props.seconds)}</span>
+            <span className="clock-child period">{this.props.period}</span>
+          </div>
+          <div onClick={() => this.props.handleClickDelete(this.props.id)}>Delete</div>
+        </div>
+
+        <AlarmClockForm
+          hours={this.props.hours}
+          minutes={this.props.minutes}
+          seconds={this.props.seconds}
+          period={this.props.period}
+          ringAlarm={this.props.ringAlarm} />
       </div>
-    )
+    );
   }
 }
 

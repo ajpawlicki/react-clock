21c9b65660fb8291436b1b4bd5f2df3dce22a595
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-11-12 15:21:00 -0800

    Change ClockList component to stateless functional component

--- react-client/src/Components/ClockList.jsx
+++ react-client/src/Components/ClockList.jsx
@@ -1,28 +1,22 @@
 import React, {Component} from 'react';
 import Clock from './Clock.jsx';
 
-class ClockList extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="clock-list">
-        {this.props.clockList.map(id => {
-          return <Clock
-            key={id}
-            id={id}
-            hours={this.props.hours}
-            minutes={this.props.minutes}
-            seconds={this.props.seconds}
-            period={this.props.period}
-            ringAlarm={this.props.ringAlarm}
-            handleClickDelete={this.props.handleClickDelete} />
-        })}
-      </div>
-    );
-  }
-}
+const ClockList = (props) => {
+  return (
+    <div className="clock-list">
+      {props.clockList.map(id => {
+        return <Clock
+          key={id}
+          id={id}
+          hours={props.hours}
+          minutes={props.minutes}
+          seconds={props.seconds}
+          period={props.period}
+          ringAlarm={props.ringAlarm}
+          handleClickDelete={props.handleClickDelete} />
+      })}
+    </div>
+  );
+};
 
 export default ClockList;
